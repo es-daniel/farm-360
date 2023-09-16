672917e3ca7734ce1a3643ec055a394005e380f2
Roy Elizondo <danielelizondo12@gmail.com>
2023-09-16 16:20:03 -0600

    feat(init-project): :sparkles: init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
+import { NavigationComponent } from '@components/navigation/navigation.component';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, NavigationComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'farm-360';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('@components/pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    loadComponent: () => import('@components/pages/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent),
+  },
+];
