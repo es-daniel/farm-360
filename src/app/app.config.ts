93e49813177f523a951654da65d0a040fc95ba1b
Roy Elizondo <danielelizondo12@gmail.com>
2023-09-24 12:51:29 -0600

    feat: :sparkles: init angular fire module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    importProvidersFrom(AngularFireModule.initializeApp(environment.firebaseConfig)),
+  ],
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCzJpkE88rx9QgKbw5YLXflxVupJPxGkBA',
+    authDomain: 'finca-360.firebaseapp.com',
+    projectId: 'finca-360',
+    storageBucket: 'finca-360.appspot.com',
+    messagingSenderId: '376210354445',
+    appId: '1:376210354445:web:586caf0073e0331d70c044',
+  },
+};
